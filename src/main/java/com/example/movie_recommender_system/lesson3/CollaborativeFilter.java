6927b7dd783f36c32708d8cd1eb6d23635bf65a2
alibek_khudoykulov <alibekxudoyqulovofficial@gmail.com>
2024-07-21 08:44:01 +0500

    Autowiring by type - @Primary

--- src/main/java/com/example/movie_recommender_system/lesson3/CollaborativeFilter.java
+++ src/main/java/com/example/movie_recommender_system/lesson3/CollaborativeFilter.java
@@ -1,7 +1,10 @@
 package com.example.movie_recommender_system.lesson3;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Component
+@Primary
 public class CollaborativeFilter implements Filter {
     public String[] getRecommendations(String movie) {
         //logic of collaborative filter
